19163522f1eedd1331e19349e3320fff2a0cd841
Srijit Paul <111722460+Srijit12@users.noreply.github.com>
2024-05-31 05:59:48 +0000

    container Changes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountID = 155443
+let accountEmail = "srijit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+// accountID = 2 // not allowed
+
+accountEmail = "Sr@sr.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
